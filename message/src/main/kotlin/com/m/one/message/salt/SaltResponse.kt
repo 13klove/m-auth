3ac2f4a97990e1a9ca485e4f701de7d9c49a8595
13klove <13klove@naver.com>
2023-06-28 22:59:34 +0900

    bbbb

--- message/src/main/kotlin/com/m/one/message/salt/SaltResponse.kt
+++ message/src/main/kotlin/com/m/one/message/salt/SaltResponse.kt
@@ -4,7 +4,7 @@ data class SaltResponse(
     var id: String,
     var salt: String,
 )
-
+ //bbb
 
 ///12321 qwerqweqwe
 ///12321 eqwewq
@@ -12,4 +12,3 @@ data class SaltResponse(
 // a1 //bb
 // a1 //aa
 
-// bbb
